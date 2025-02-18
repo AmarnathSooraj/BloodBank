1ba1a03e7eeca102db9793c6ee510ad49820d806
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-02-18 16:42:41 +0530

    ejfk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,16 @@
 import React, { useState } from 'react';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
-import tt from '../assets/testtube.png';
-
+import { Link } from 'react-router-dom' 
+import tt from '../assets/testtube.png'
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
+
     <nav className="nav">
       <div className='logo'>
-        <span><img src={tt} alt="Logo" />Haemax</span>
+        <span><img src={tt}/>Haemax</span>
       </div>
-      <div className="hamburger" onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-      <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+      <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link>Walkthrough</Link></li>
         <li><Link to='/about'>About Us</Link></li>
