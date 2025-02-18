c4de2de979aae4cbce4955c993b270b4e4d1a820
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-02-18 20:52:51 +0530

    fd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Navbar.css';
-import { Link } from 'react-router-dom' 
-import tt from '../assets/testtube.png'
+import { Link, useLocation } from 'react-router-dom';
+import tt from '../assets/testtube.png';
+
 function Navbar() {
+  const location = useLocation();
+
+  // If the current path is not the home page, all links should be red
+  const isNotHome = location.pathname !== '/';
 
   return (
-
     <nav className="nav">
       <div className='logo'>
-        <span><img src={tt}/>Haemax</span>
+        <span><img src={tt} alt="logo" />Haemax</span>
       </div>
       <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link>Walkthrough</Link></li>
-        <li><Link to='/about'>About Us</Link></li>
-        <li><Link>Explore</Link></li>
+        <li>
+          <Link to='/' className={isNotHome ? 'active-link' : ''}>Home</Link>
+        </li>
+        <li>
+          <Link to='/' className={isNotHome ? 'active-link' : ''}>Walkthrough</Link>
+        </li>
+        <li>
+          <Link to='/about' className={isNotHome ? 'active-link' : ''}>About Us</Link>
+        </li>
+        <li>
+          <Link to='/' className={isNotHome ? 'active-link' : ''}>Explore</Link>
+        </li>
       </ul>
       <button className='signup-btn'>Sign Up</button>
     </nav>
@@ -22,3 +34,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
